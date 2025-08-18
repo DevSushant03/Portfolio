fec32c33c358e4432686324b017b2cd74eee1c8c
nachanekarsiddh22 <nachanekarsiddh22@gmail.com>
2025-08-18 23:58:19 +0530

    add project

--- src/API/projectData.js
+++ src/API/projectData.js
@@ -1,6 +1,8 @@
 import weatherAppImg from "/src/assets/images/weatherApp.png";
 import atlasImg from "/src/assets/images/atlasProject.png"
 import Quink from "/src/assets/images/urlShortner.png"
+import ViseVerse from "/src/assets/images/viseverse.png"
+
 
 
 const project = [
@@ -11,6 +13,14 @@ const project = [
     "image": weatherAppImg,
     "liveDemo": "https://climatora.netlify.app/",
     "githubRepo": "https://github.com/DevSushant03/WeatherGlobe"
+  },
+  {
+    "name": "ViseVerse",
+    "description": "ViseVerse is a smart web app & Chrome extension that extracts text from images using OCR and enhances it with AI and export to PDF/DOCX/TXT.",
+    "technologies": ["React.js", "Node.js","tesserect.js"],
+    "image": ViseVerse,
+    "liveDemo": "https://viseverse.netlify.app/",
+    "githubRepo": "https://github.com/DevSushant03/ViseVerse"
   },
 
   {
